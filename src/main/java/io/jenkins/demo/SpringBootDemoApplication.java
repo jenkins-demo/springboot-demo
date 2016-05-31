2f52707b9fcb9aba98f128c464727dd4b542250e
Arnaud HERITIER <aheritier@apache.org>
2016-05-31 11:34:25 +0200

    Import SpringBoot App skeleton

---
+++ src/main/java/io/jenkins/demo/SpringBootDemoApplication.java
@@ -0,0 +1,12 @@
+package io.jenkins.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoApplication.class, args);
+	}
+}
